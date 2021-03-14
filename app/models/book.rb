141fdba7c9b7ed129e441faea10e1d564f015660
victorzottmann <zottmann.victor@gmail.com>
2021-03-14 20:55:44 +1100

    setup code for author, book, category, publisher and user models

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,11 @@ class Book < ApplicationRecord
   belongs_to :category
   belongs_to :publisher
   belongs_to :user
+
+  has_one_attached :picture
+
+  enum condition: {damaged: 0, used: 1, good: 2, excellent: 3}
+
+  accepts_nested_attributes_for :author
+  accepts_nested_attributes_for :publisher
 end
