fbd20e8bc39da429d6c72af1143413020001607b
victorzottmann <zottmann.victor@gmail.com>
2021-03-15 21:14:49 +1100

    fix bug that deleted cart when a single book was deleted

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,6 +9,28 @@ class CartsController < ApplicationController
 
   
   def show
+    
+    # if user_signed_in?
+    #   session = Stripe::Checkout::Session.create(
+    #     payment_method_types: ['card'],
+    #     customer_email: current_user.email,
+    #     line_items: [{
+    #       name: line_item.book.title,
+    #       amount: (@book.price * 100),
+    #       currency: 'aud',
+    #       quantity: 1
+    #     }],
+    #     payment_intent_data: {
+    #       metadata: {
+    #         user_id: current_user.id,
+    #         book_id: @book.id
+    #       }
+    #     },
+    #     success_url: "#{root_url}payments/success?bookId=#{@book.id}",
+    #     cancel_url: "#{root_url}books"
+    #   )
+    #   @session_id = session.id
+    # end
   end
 
 
